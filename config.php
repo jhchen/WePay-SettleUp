f95b9997d820154e9f1001e697cc7e62867fed51
Jason Chen <jhchen7@gmail.com>
2011-01-11 03:37:02 -0800

    added skeleton config file

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+
+define('WEPAY_KEY', 'Your WePay Key'); 
+define('WEPAY_SECRET', 'Secret'); 
+define('WEPAY_OAUTH_TOKEN', 'Your Oauth token'); 
+define('WEPAY_OAUTH_SECRET', 'Also a secret'); 
+define('WEPAY_API_URL', 'https://wepayapi.com/v1/');
